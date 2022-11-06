43b09e6ee4188e696a49a1a177e0e4f06097170b
Apkumar7 <115951383+Apkumar7@users.noreply.github.com>
2022-11-06 10:18:10 -0600

    Add files via upload
    This is my first python file

---
+++ main.py
@@ -0,0 +1,318 @@
+print("                      TRIVIA QUIZ ")
+ #1st section of test  
+print("                   GEOGRAPHY SECTION")
+
+Score = 0
+user_answer1 = input(print(" What city is the tallest USA state capital building located in?"))
+if user_answer1 == ("Austin"):
+    print(user_answer1 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer1 + " is incorrect")
+    
+    
+    
+#2nd question of test    
+user_answer2= input(print("What 2 buildings collapsed because of the events of 9/11? put ' The' in front of the name"))
+
+if user_answer2 == ("The Twin Towers"):
+    print(user_answer2 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer2 + " is incorrect")
+    
+    
+    
+    
+    
+ #this is the 3rd question of the test   
+user_answer3= input(print("What is the oldest building in the USA? (Put 'the' in front of the name)"))
+
+if user_answer3 == ("The Fairbanks House"):
+    print(user_answer3 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer3 + " is incorrect")
+    
+    
+print("Your score is ", Score)
+    
+print("ANIMALS SECTION")     
+
+    
+    
+user_answer4= input(print("What animal has the strongest bite? (singular)"))
+
+if user_answer4 == ("A Hippopotamus"):
+    print(user_answer4 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer4 + " is incorrect")
+    
+    
+   
+    
+    
+user_answer5 = input(print("What is the known name of this  animal Panthera tigris"))
+                    
+if user_answer5 == ("Tiger"):
+    print(user_answer5 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer5 + " is incorrect")    
+
+                    
+                                   
+                    
+            
+user_answer6= input(print("What animal species is the most common in the world besides humans?"))
+                    
+                    
+if user_answer6== ("Rats"):
+    print(user_answer6 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer6 + " is incorrect")
+                    
+print("Your score is ", Score)
+
+print("MATH SECTION")
+        
+user_answer7= input(print("What city was geomitry created in?"))
+
+                    
+if user_answer7 == ("Athens"):
+    print(user_answer7 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer7 + " is incorrect")
+    
+    
+user_answer8= input(print("What theorem is used to find the hypotonus of a triangle"))
+                    
+if user_answer8== ("The Pythagorean Theorem"):
+    print(user_answer8 +" is correct") 
+    Score= Score+1
+else:
+    print(user_answer8 + " is incorrect")
+    
+    
+user_answer9= input(print("In math what operation is repeated addition?"))
+
+                    
+if user_answer9 == ("Multiplication"):
+    print(user_answer9 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer9 +" is incorrect")
+    
+print("Your score is ", Score)    
+    
+print("WORLD WONDERS SECTION")
+
+user_answer10= input(print("what world wonder is located in Eygypt?"))
+
+                     
+if user_answer10== ("The Great Pyramid of Giza"):
+    print(user_answer10 +" is correct")
+    Score= Score+1
+else:
+    print(user_answer10 + " is incorrect")
+
+    
+    
+user_answer11= input(print("What world wonder's second word sounds like a electric type pokemon?"))
+
+
+
+
+if user_answer11== ("Machu Picchu"):
+    print(user_answer11 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer11 +" is incorrect")
+    
+    
+    
+    
+user_answer12= input(print("What World Wonder is located in India?"))
+
+
+if user_answer12== ("The Taj Mahal"):
+    print(user_answer12 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer12 + " is incorrect")
+    
+print("Your score is ", Score)  
+    
+print("SCIENCE SECTION")    
+    
+    
+    
+user_answer13 = input(print("What is the name of the 'evil brother' of the elephants toothpaste experiment? "))
+
+
+
+if user_answer13== ("Devils toothpaste"):
+    print(user_answer13 + "is correct")
+    Score= Score+1
+else:
+    print(user_answer13 +" is incorrect")
+    
+    
+user_answer14 = input(print("What gadget are all science labs required to have?"))
+
+
+
+
+if user_answer14== ("An eyewash station"):
+    print(user_answer14 + " is correct")
+    Score= Score+1
+    
+else:
+    print(user_answer14 + " is incorrect")
+    
+
+    
+user_answer15 = input(print("What is the act of a animal makeing its own light called?"))
+
+
+    
+    
+if user_answer15 == (" bioluminesence"):
+    print(user_answer15 +" is correct")
+    Score= Score+1
+    
+else:
+    print(user_answer15 +" is incorrect")
+    
+    
+    
+print("Your score is ", Score)    
+    
+    
+print("ELECTRONICS SECTION")
+
+
+
+user_answer16= input(print("What year did the first iphone come out"))
+
+if user_answer16 == ("2017"):
+    print(user_answer16 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer16 +" is incorrect")
+    
+
+    
+user_answer17= input(print("What company created the iphone"))
+
+
+
+if user_answer17 == ("Apple"):
+    print(user_answer17 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer17 + " is incorrect")
+    
+    
+
+user_answer18= input(print("What Iphone company is Apple's main comptetor?"))
+
+
+
+if user_answer18== ("Samsung"):
+    print(user_answer18 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer18 + " is incorrect")
+    
+    
+    
+print("Your score is ", Score)    
+    
+print("LANGUAGE SECTION")
+
+
+
+user_answer19=input(print("What language is spoken most worldwide?"))
+
+
+if user_answer19== ("English"):
+    print(user_answer19 +" is correct")
+    Score= Score+1
+else:
+    print(user_answer19 +" is incorrect")
+    
+    
+user_answer20= input(print("What language is spoken the most in Austrailia?"))
+    
+    
+if user_answer20== ("English"):
+    print(user_answer20+" is correct")
+    Score= Score+1
+else:
+    print(user_answer20+ " is incorrect")
+    
+    
+    
+user_answer21=input(print("What is the first name of the person who holds the world record for most languages spoken fluently"))
+
+
+
+if user_answer21==("Powell"):
+    print(user_answer21 +" is correct")
+    Score= Score+1
+else:
+    print(user_answer21 +" is incorrect")
+    
+    
+print("Your score is ", Score)
+
+print("APP DOWNLOADS")
+
+
+user_answer22= input(print("What was the 2nd most downloaded app in 2020? It makes sense when you think about it."))
+
+
+if user_answer22==("Zoom"):
+    print(user_answer22 + " is correct")
+    Score= Score+1
+else:
+    print(user_answer22 + " is incorrect")
+    
+    
+user_answer23=input(print("What was the most downladed app of 2021?"))
+
+
+
+if user_answer23==("TikTok"):
+    print(user_answer23+" is correct")
+    Score= Score+1
+else:
+    print(user_answer23+ " is incorrect")
+    
+    
+    
+
+user_answer24= input(print("What is the most downloaded app of 2014?"))
+
+
+if user_answer24==("Facebook"):
+    print(user_answer24+" is correct")
+    Score= Score+1
+else:
+    print(user_answer24 + " is incorrect")
+
+user_answer25= input(print("WHo was the president when George H Bush was vice president?"))
+
+if user_answer25==("Ronald Reagan")
+  print(user_answer25)+ "is correct")
+  Score=Score+1
+    
+else:    
+  print(user_answer25 + "is incorrect")
+print("Your final score is ", Score)
+ #this is where the points and responces for points are created   
+Score = Score +1
